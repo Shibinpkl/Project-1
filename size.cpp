fdd71bd602c75753122ceaeb5afaec6159321092
Shibinpkl <99410930+Shibinpkl@users.noreply.github.com>
2022-02-21 11:39:54 +0530

    Create size.cpp
    Developed by Shibin
    This program prints size of char, int, float, double

---
+++ size.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Size of char:1 byte \n";
+    cout<<"Size of int:4 byte \n";
+    cout<<"Size of float:4 byte \n";
+    cout<<"Size of double:8 byte";
+    return 0;
+}
