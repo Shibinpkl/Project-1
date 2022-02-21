8f439f3c1a5628c19f2f3be5aab7accc3f677209
Shibinpkl <99410930+Shibinpkl@users.noreply.github.com>
2022-02-21 11:45:52 +0530

    Create Product.cpp
    This program gives product any two numbers entered by the user

---
+++ Product.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout<<"Enter two numbers";
+    cin>>a>>b;
+    cout<<"Product of the numbers is"<<a*b; 
+    return 0;
+}
